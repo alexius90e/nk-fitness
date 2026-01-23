538078c12b0bb42a204d241ae31b7f192b09ed3b
Aliaksei Karzhou <alexius90e@gmail.com>
2026-01-23 19:49:35 +0300

    feat: added header

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
+// header
+
+const headerEl = document.querySelector('.header');
+const headerControlsEl = document.querySelector('.header__controls');
+
+if (headerEl) {
+  let lastScrollTop = 0;
+  let ticking = false;
+
+  function updateHeader(scrollTop) {
+    if (scrollTop < 200) {
+      headerEl.classList.remove('active', 'hidden');
+    } else if (scrollTop >= 200 && scrollTop <= 240) {
+      headerEl.classList.remove('active');
+      headerEl.classList.add('hidden');
+    } else {
+      headerEl.classList.add('hidden');
+      headerEl.classList.add('active');
+    }
+  }
+
+  window.addEventListener('scroll', () => {
+    lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        updateHeader(lastScrollTop);
+        ticking = false;
+      });
+      ticking = true;
+    }
+  });
+}
+
+if (headerControlsEl) {
+  headerControlsEl.addEventListener('click', (event) => {
+    const isLoginBtn = event.target.classList.contains('header__controls-login');
+
+    if (isLoginBtn) event.currentTarget.classList.add('header__controls_logged');
+  });
+}
+
+// first-screen
+
 const firstScreenSwiperEl = document.querySelector('.first-screen .swiper');
 
 if (firstScreenSwiperEl) {
